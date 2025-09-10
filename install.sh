b1b47f65fb9576d06a82fe989a962a33a26d84be
Darrel Malkin <catrassyt@gmail.com>
2025-09-10 19:13:05 +1000

    chore: add some formatting to install script output

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # Make the directory
-echo "Making directory to store script..."
+echo "  Making directory to store script..."
 mkdir -p /opt/nixos-lxc
 
 # Get nix config file
-echo "Downloading Nix Config..."
+echo "  Downloading Nix Config..."
 curl -fsSL -o /opt/nixos-lxc/configuration.nix https://raw.githubusercontent.com/CatRass/nixos-lxc/refs/heads/main/src/configuration.nix
 
 # Get the environment file
-echo "Downloading environment file..."
+echo "  Downloading environment file..."
 curl -fsSL -o /opt/nixos-lxc/lxc.env https://raw.githubusercontent.com/CatRass/nixos-lxc/refs/heads/main/src/lxc.env
 
 read -p 'Start installation straight away? (Y/n) ' immediateInstall
 
 if [[ ${immediateInstall} == [yY] || -z ${immediateInstall} ]]; then
-  echo "Starting install..."
+  echo "  Starting install..."
   curl -fsSL -o /opt/nixos-lxc/create.sh https://raw.githubusercontent.com/CatRass/nixos-lxc/refs/heads/main/src/script.sh
   cd /opt/nixos-lxc
   bash ./create.sh
 elif [[ ${immediateInstall} == [nN] ]]; then
-  echo "Downlaoding and exiting..."
+  echo "  Downlaoding and exiting..."
   curl -fsSL -o /opt/nixos-lxc/create.sh https://raw.githubusercontent.com/CatRass/nixos-lxc/refs/heads/main/src/script.sh
-  echo "To install, run 'bash /opt/nixos-lxc/create.sh'"
+  echo "  To install, run 'bash /opt/nixos-lxc/create.sh'"
 fi
