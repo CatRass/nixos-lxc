bd2af18281ad1aea8b2457c29b40f8d3358c12c6
Darrel Malkin <catrassyt@gmail.com>
2025-09-10 19:03:02 +1000

    refactor: move all files except install script to src folder

--- install.sh
+++ install.sh
@@ -4,19 +4,19 @@
 mkdir -p /opt/nixos-lxc
 
 # Get nix config file
-curl -fsSL -o /opt/nixos-lxc/configuration.nix https://raw.githubusercontent.com/CatRass/nixos-lxc/refs/heads/main/configuration.nix
+curl -fsSL -o /opt/nixos-lxc/configuration.nix https://raw.githubusercontent.com/CatRass/nixos-lxc/refs/heads/main/src/configuration.nix
 
 # Get the environment file
-curl -fsSL -o /opt/nixos-lxc/lxc.env https://raw.githubusercontent.com/CatRass/nixos-lxc/refs/heads/main/lxc.env
+curl -fsSL -o /opt/nixos-lxc/lxc.env https://raw.githubusercontent.com/CatRass/nixos-lxc/refs/heads/main/src/lxc.env
 
 read -p 'Start installation straight away? (Y/n) ' immediateInstall
 
 if [[ ${immediateInstall} == [yY] || -z ${immediateInstall} ]]; then
   echo "Starting install..."
-  curl -fsSL -o /opt/nixos-lxc/create.sh https://raw.githubusercontent.com/CatRass/nixos-lxc/refs/heads/main/script.sh
+  curl -fsSL -o /opt/nixos-lxc/create.sh https://raw.githubusercontent.com/CatRass/nixos-lxc/refs/heads/main/src/script.sh
   cd /opt/nixos-lxc
   bash ./create.sh
 elif [[ ${immediateInstall} == [nN] ]]; then
   echo "Downlaoding and exiting..."
-  curl -fsSL -o /opt/nixos-lxc/create.sh https://raw.githubusercontent.com/CatRass/nixos-lxc/refs/heads/main/script.sh
+  curl -fsSL -o /opt/nixos-lxc/create.sh https://raw.githubusercontent.com/CatRass/nixos-lxc/refs/heads/main/src/script.sh
 fi
