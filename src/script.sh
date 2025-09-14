d0ad0bb266bc08bc03103fa2d8af5c375504179d
Darrel Malkin <catrassyt@gmail.com>
2025-09-14 14:01:41 +1000

    feat: add error handling for LXC creation

--- src/script.sh
+++ src/script.sh
@@ -48,20 +48,27 @@ wget -O /var/lib/vz/template/cache/nixos-system.tar.xz \
   --no-verbose \
   https://hydra.nixos.org/job/nixos/release-24.11/nixos.proxmoxLXC.x86_64-linux/latest/download-by-type/file/system-tarball
 
-export errorMsg=$?
-if [ "$errorMsg" != "0" ]; then
+export imageDownloadError=$?
+if [ "$imageDownloadError" != "0" ]; then
     echo "  Error: Unable to download NixOS template"
     exit 1
 fi
 
 # Create the LXC
-pct create ${ctid} ${ctt} \
+lxcCreate=$(pct create ${ctid} ${ctt} \
   --hostname=${ctname} \
   --ostype=nixos --unprivileged=0 --features nesting=1 --start=${ctstart}\
   --net0 name=eth0,bridge=vmbr0,ip=dhcp \
   --arch=amd64 --swap=${ctswap} --memory=${ctram} \
   --cores=${ctcpu} \
-  --storage=${cts}
+  --storage=${cts} \
+  2>&1)
+
+export lxcCreateError=$?
+if [ "$lxcCreateError" != "0" ]; then
+    echo "  Error: Unable to create LXC, reason: \"${lxcCreate}\""
+    exit 1
+fi
 
 # Change some thingies
 pct resize ${ctid} rootfs +2G
