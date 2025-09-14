1826379ccdcdc5a18270e54f27580fe807badca8
Darrel Malkin <catrassyt@gmail.com>
2025-09-14 16:30:14 +1000

    feat: add error handling to current install script (#2)

--- src/script.sh
+++ src/script.sh
@@ -17,6 +17,14 @@ cleanupEnv() {
   unset nixos_autostart
 }
 
+deleteUnfinishedLXC() {
+  local lxcID=$1
+  local shutdown=${$2:-false}
+  if $shutdown; then
+    pct shutdown ${lxcID}
+  fi
+  pct destroy ${lxcID}
+}
 
 clear
 
@@ -42,42 +50,113 @@ ctcpu=${nixos_cpu:-2}
 ctswap=${nixos_swap:-1024}
 ctstart=${nixos_autostart:-1}
 
-# Download the image
-echo "Downloading latest 24.11 LXC template . . ."
-wget -O /var/lib/vz/template/cache/nixos-system.tar.xz \
-  --no-verbose \
-  https://hydra.nixos.org/job/nixos/release-24.11/nixos.proxmoxLXC.x86_64-linux/latest/download-by-type/file/system-tarball
+if [ ! -e "/opt/nixos-lxc/rerun.tmp" ]; then
 
-export errorMsg=$?
-if [ "$errorMsg" != "0" ]; then
-    echo "  Error: Unable to download NixOS template"
+  # Download the image
+  echo "Downloading latest 24.11 LXC template . . ."
+  wget -O /var/lib/vz/template/cache/nixos-system.tar.xz \
+    --no-verbose \
+    https://hydra.nixos.org/job/nixos/release-24.11/nixos.proxmoxLXC.x86_64-linux/latest/download-by-type/file/system-tarball
+
+  export imageDownloadError=$?
+  if [ "$imageDownloadError" != "0" ]; then
+      echo "  Error: Unable to download NixOS template"
+      exit 1
+  fi
+
+  # Create the LXC
+  lxcStats=`cat << EOF
+  Creating NixOS LXC '${ctname}':
+    - ID: ${ctid}
+    - Cores: ${ctcpu}
+    - RAM: ${ctram}
+    - SWAP: ${ctswap}
+    - Auto Start: ${ctstart}`
+  echo "${lxcStats}"
+
+  lxcCreate=$(pct create ${ctid} ${ctt} \
+    --hostname=${ctname} \
+    --ostype=nixos --unprivileged=0 --features nesting=1 --start=${ctstart}\
+    --net0 name=eth0,bridge=vmbr0,ip=dhcp \
+    --arch=amd64 --swap=${ctswap} --memory=${ctram} \
+    --cores=${ctcpu} \
+    --storage=${cts} \
+    2>&1)
+
+  export lxcCreateError=$?
+  if [ "$lxcCreateError" != "0" ]; then
+      echo "  Error: Unable to create LXC, reason: \"${lxcCreate}\""
+      exit 1
+  fi
+
+  # Resize File System
+  echo "Resizing File System . . ."
+  lxcResize=$(pct resize ${ctid} rootfs +2G 2>&1)
+
+  export lxcResizeError=$?
+  if [ "$lxcResizeError" != "0" ]; then
+      echo "  Error: Unable to resize LXC, reason: \"${lxcResize}\""
+      echo "  Deleting LXC..."
+      deleteUnfinishedLXC ${ctid}
+      exit 1
+  fi
+
+  # Start the LXC
+  echo "Starting LXC . . ."
+  lxcStart=$(pct start ${ctid} 2>&1)
+
+  export lxcStartError=$?
+  if [ "$lxcStartError" != "0" ]; then
+      echo "  Error: Unable to start LXC, reason: \"${lxcStart}\""
+      echo "  Deleting LXC..."
+      deleteUnfinishedLXC ${ctid}
+      exit 1
+  fi
+
+  # Remove password
+  echo "Removing default root password . . ."
+  nixosRemPass=$(pct exec ${ctid} -- sh -c "source /etc/set-environment && passwd --delete root")
+
+  export nixosRemPassError=$?
+  if [ "$nixosRemPassError" != "0" ]; then
+      echo "  Error: Unable remove default password, reason: \"${nixosRemPass}\""
+      exit 1
+  fi
+fi
+
+# Get NixOS Updates
+echo "Updating NixOS . . ."
+nixosUpdate=$(pct exec ${ctid} -- sh -c "source /etc/set-environment && nix-channel --update" 2>&1)
+
+export nixosUpdateError=$?
+if [ "$nixosUpdateError" != "0" ]; then
+    echo "  Error: Unable to update NixOS LXC, reason: \"${nixosUpdate}\""
     exit 1
 fi
 
-# Create the LXC
-pct create ${ctid} ${ctt} \
-  --hostname=${ctname} \
-  --ostype=nixos --unprivileged=0 --features nesting=1 --start=${ctstart}\
-  --net0 name=eth0,bridge=vmbr0,ip=dhcp \
-  --arch=amd64 --swap=${ctswap} --memory=${ctram} \
-  --cores=${ctcpu} \
-  --storage=${cts}
+# Move NixOS config from this directory to the lxc
+echo "Moving Nix config to LXC . . ."
+lxcPushConfig=$(pct push ${ctid} /opt/nixos-lxc/configuration.nix /etc/nixos/configuration.nix)
 
-# Change some thingies
-pct resize ${ctid} rootfs +2G
-
-# Start the LXC
-pct start ${ctid}
-
-# Move config from this directory to the lxc
-pct push ${ctid} /opt/nixos-lxc/configuration.nix /etc/nixos/configuration.nix
-
-# Remove password
-pct exec ${ctid} -- sh -c "source /etc/set-environment && passwd --delete root"
+export lxcConfigError=$?
+if [ "$lxcConfigError" != "0" ]; then
+    echo "  Error: Unable to push NixOS config, reason: \"${lxcPushConfig}\""
+    exit 1
+fi
 
 # Build the nixos from config
-pct exec ${ctid} -- sh -c "source /etc/set-environment && nix-channel --update"
-pct exec ${ctid} -- sh -c "source /etc/set-environment && nixos-rebuild switch --upgrade"
+echo "Rebuilding NixOS with Config . . ."
+nixosBuild=$(pct exec ${ctid} -- sh -c "source /etc/set-environment && nixos-rebuild switch --upgrade" 2>&1)
+
+export nixosBuildError=$?
+if [ "$nixosBuildError" != "0" ]; then
+    echo "  Error: Unable to build NixOS Config, reason: \"${nixosBuild}\""
+    echo "  To re-attempt the install, please re-run this script with 'bash /opt/nixos-lxc/create.sh'"
+    touch /opt/nixos-lxc/rerun.tmp
+    exit 1
+else
+  rm -f /opt/nixos-lxc/rerun.tmp
+fi
 
 # clean up environmnet
 cleanupEnv
